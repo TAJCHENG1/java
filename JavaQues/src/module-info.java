c999da22906fb2df663bc0d98b09339eebf28f96
sytajudd <sytajudd@LIN65006971.corp.capgemini.com>
2022-08-01 21:17:12 +0530

    First commit

---
+++ JavaQues/src/module-info.java
@@ -0,0 +1,130 @@
+/*FEATURES - Simple,object-oriented,portable,platformindependent,secured etc,.
+ 
+ *OOPS - It is like having data and methods defined in the class to which it belongs creating
+        objects that contain both data and methods. 
+  
+ *IF - if a boolean expression evaluates to true the body of if statement will executed.
+       if a boolean expression evaluates to false the body of if statement will skipped. 
+  
+ *IF ELSE - if a boolean_expression evaluates to true the body of if statement will executed.
+            if a boolean_expression evaluates to false the body of else statement will executed. 
+  
+ *IF ELSE IF -  The if_else_if statement allows us to make a choice from two options , if you need to 
+                make a choice from more than 2options, we can use another else part in the 
+                if else statement. 
+ 
+ *WHILE - Body of while loop is executed as long as the boolean expression is true. 
+ 
+ *DO-WHILE - If the no.of iteration is not fixed and u must have to execute the loop atleast once it is 
+             recommended to use while loop 
+  
+ *FORLOOP -  It is a control flow statement that iterates a part of the programs multiple times. 
+ *
+ *
+ *
+ * 
+ * 
+                                       ****OOPS****  
+                                          
+ * OOP is for creating objects that contain both data and methods                                                                                                           
+ * OOPS - It is like having data and methods defined in the class to which it belongs creating
+        objects that contain both data and methods. 
+                                           
+ * INHERITANCE -  It is a way of creating a new class for using details of an existing class 
+                without modifying it.      4--types.
+                
+       single - Every element in class a derived from class b ,
+       multi - From 2level class created 1,
+       herarchical - from2 class created 1-class,
+       multiple - From a single class created multiple.
+       
+ * ENCAPSULATION - Every java class is an example of encapsulation because we write within class.
+                  Using oops in java we restrict access to methods and variables .
+                  This prevents data from direct modification is known as encap.
+                  
+ * POLYMORPHISM - It is like a common interface for multiple forms(data types),
+                 suppose we need to color a shape, there are multiple shape options
+                 (rectangle,square,circle).however we could use the same method to color any
+                  shape this concept is known as polymorphism.                                 
+  
+ * ABSTRACTION - Abstraction allows displaying only the important information and hiding
+                 the implementation details. for example: while riding a bike, u know that
+                 if u raise the accelerator, the speed will increase, but u dont know how it
+                 actually happens .this is data abstraction as the implementation details 
+                 are hidden from the rider.
+ 
+ * INTERFACE - it allows us to declare methods without defining them. any class that implements
+              an interface defines the methods of the interface. 
+  
+ * CLASS - A CLASS IS A USER DEFINED BLUEPRINT OR PROTOTYPE FROM WHICH OBJECTS ARE CREATED. 
+  
+ * OBJECT -  AN OBJECT IS AN INSTANCE OF A CLASS. IT IS A REAL WORLD ENTITY. 
+  
+  
+  
+                                   
+                                     ****EXCEPTION****
+                                     
+ * EXCEPTIONHANDLING - Exception Handling is a mechanism to handle runtime errors such as 
+                       ClassNotFoundException, IOException, SQLException, RemoteException, etc.
+  
+ * EXCEPTION - It is a kind of notification that interrupts the normal execution of a program. 
+ 
+ * CHECKED - The exception which are checked by compiler for smooth execution of the program
+            at runtime. using (try,catch) checked excep should be handled.
+     
+ * UNCHECKED - The exception which are not checked by compiler whether the programmer handling or not
+              such type of exception by default consider as unchecked.           
+  
+ * TRY/CATCH - its used to handle exceptions. The try block defines a set of statements that may
+              lead to an error. The catch block basically catches the exception. 
+  
+ * FINALLY - Finally contains the block of code that must be executed whether
+            an exception occurs or not. 
+ 
+ * FINAL - It is used with the classes, methods and variables.
+ 
+ * FINALIZE - Used to perform cleanup processing just before object garbage is collected.
+ 
+ * THROW - The throw keyword is mainly used to throw custom exceptions.
+  
+ * THROWS - Used to indicate what exception type may be thrown by a method. 
+  
+ * CONSTRUCTOR - It is a method used to initialize objects. The constructor is called when an object 
+                 of a class is created. It can be used to set initial values for object attributes. 
+                 In java, a constructor is a block of codes similar to the method. 
+                 
+                 Constructor is used to create an object.
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
